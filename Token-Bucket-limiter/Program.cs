28b4c989b2ea5579346b834ae1b55ef13065a61a
hakan.basturknewinn.com <forxynet@gmail.com>
2024-05-07 10:32:36 +0300

    updated token limiter

--- Token-Bucket-limiter/Program.cs
+++ Token-Bucket-limiter/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+using WebRateLimitAuth.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var tokenPolicy = "token";
+var myOptions = new MyRateLimitOptions();
+builder.Configuration.GetSection(MyRateLimitOptions.MyRateLimit).Bind(myOptions);
+
+builder.Services.AddRateLimiter(_ => _ 
+    .AddTokenBucketLimiter(policyName: tokenPolicy, options => {
+        options.TokenLimit = myOptions.TokenLimit;
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = myOptions.QueueLimit;
+        options.ReplenishmentPeriod = TimeSpan.FromSeconds(myOptions.ReplenishmentPeriod);
+        options.TokensPerPeriod = myOptions.TokensPerPeriod;
+        options.AutoReplenishment = myOptions.AutoRepleinsment;
+    }));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRateLimiter();
+
+static string GetTicks() => (DateTime.Now.Ticks & 0x11111).ToString("00000");
+
+app.MapGet("/", () => Results.Ok( $"Token Limiter {GetTicks()}")).RequireRateLimiting(tokenPolicy);
 
 app.Run();
