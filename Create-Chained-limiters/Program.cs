62ff3f146210550a0d53fb4dcce51901d2945f0a
hakan.basturknewinn.com <forxynet@gmail.com>
2024-05-09 23:42:46 +0300

    updated create chained limiters

--- Create-Chained-limiters/Program.cs
+++ Create-Chained-limiters/Program.cs
@@ -1,6 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Globalization;
+using System.Threading.RateLimiting;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRateLimiter(_ =>
+{
+    _.OnRejected = (context, _) =>
+    {
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            context.HttpContext.Response.Headers.RetryAfter =
+                ((int) retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+        }
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        context.HttpContext.Response.WriteAsync("Too many requests. Please try again later.");
+
+        return new ValueTask();
+    };
+    _.GlobalLimiter = PartitionedRateLimiter.CreateChained(
+        PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        {
+            var userAgent = httpContext.Request.Headers.UserAgent.ToString();
+            
+            return RateLimitPartition.GetFixedWindowLimiter
+            (userAgent, _ =>
+                new FixedWindowRateLimiterOptions
+                {
+                    AutoReplenishment = true,
+                    PermitLimit = 20,
+                    Window = TimeSpan.FromSeconds(30)
+                });
+        }));
+    });
+    
+
+var app = builder.Build();
+app.UseRateLimiter();
+
+static string GetTicks() => (DateTime.Now.Ticks & 0x11111).ToString("00000");
+
+app.MapGet("/", () => Results.Ok($"Create Chanined Limiter {GetTicks()}"));
 
 app.Run();
