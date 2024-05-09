ba6381e57fc1525d1056fc7e426e8bfb4013802e
hakan.basturknewinn.com <forxynet@gmail.com>
2024-05-09 23:15:23 +0300

    updated concurrency Limiter

--- Concurrency-limiter/Program.cs
+++ Concurrency-limiter/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+using WebRateLimitAuth.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var concurrencyPolicy = "Concurrency";
+var myOptions = new MyRateLimitOptions();
+builder.Configuration.GetSection(MyRateLimitOptions.MyRateLimit).Bind(myOptions);
+
+builder.Services.AddRateLimiter(_ => _ 
+    .AddConcurrencyLimiter(policyName: concurrencyPolicy, options => {
+        options.PermitLimit = myOptions.PermitLimit;
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = myOptions.QueueLimit;
+    }));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRateLimiter();
+
+static string GetTicks() => (DateTime.Now.Ticks & 0x11111).ToString("00000");
+
+app.MapGet("/", async () => {
+    await Task.Delay(500);
+    return Results.Ok($"Concurrency Limiter {GetTicks()}");
+});
 
 app.Run();
